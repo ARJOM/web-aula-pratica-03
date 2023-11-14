c43f1a6bded2c15c2cd95ec86a924888d9ebe997
LopesLs <lopes.carlos.host@gmail.com>
2023-11-14 11:02:05 -0300

    (feat): new model and CBV change

--- studentManagement/views.py
+++ studentManagement/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import StudentForm
+from .models import Student
 
-from studentManagement.models import Student
+class StudentListRequestHandler(View):
+    def get(self, request):
+        return render(request, 'list.html', {'students': Student.objects.all()})
 
-
-def list_view(request):
-    items = Student.objects.all()
-    return render(request, 'list.html', {'items': items})
+class StudentCreateRequestHandler(View):
+    def get(self, request):
+        return render(request, 'form.html', {'form' : StudentForm})
+    
+    def post(self, request):
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+        else:
+            return render(request, 'form.html', {'form' : form})
